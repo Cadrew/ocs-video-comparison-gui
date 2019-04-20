2a2aa9695109579acf918edbd57d8644f53deac1
adrien canino <adrien canino>
2019-04-20 19:12:12 +0200

    Basic comparison without UX

--- ocs_video.js
+++ ocs_video.js
@@ -19,7 +19,7 @@ function createWindow() {
 
 app.on('ready', () => {
 	createWindow();
-	mainWindow.setSize(1280, 740);
+	mainWindow.setSize(580, 740);
 	mainWindow.loadURL(`file:${__dirname}/index.html`); //Absolute path
 	mainWindow.center();
 });
