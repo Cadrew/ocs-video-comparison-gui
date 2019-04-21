4927e1d27d41a97e45d6b4caddfc5aef124c1bbd
adrien canino <adrien canino>
2019-04-21 10:58:22 +0200

    Ergonomics

--- ocs_video.js
+++ ocs_video.js
@@ -6,7 +6,7 @@ let mainWindow;
 
 function createWindow() {
 	mainWindow = new BrowserWindow({
-		icon: 'images/logo.png',
+		icon: 'assets/icons/logo.png',
 		movable: true,
 		backgroundColor: '#1D1D28', //In order to disable the white background color when we resize the window (instead of 'transparent: true')
 		frame: false //Borderless
@@ -19,7 +19,7 @@ function createWindow() {
 
 app.on('ready', () => {
 	createWindow();
-	mainWindow.setSize(580, 540);
+	mainWindow.setSize(525, 300);
 	mainWindow.loadURL(`file:${__dirname}/index.html`); //Absolute path
 	mainWindow.center();
 });
