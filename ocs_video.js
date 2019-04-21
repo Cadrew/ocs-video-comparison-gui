04d5e6f8f5325bc08ff4423c2e1924ae5dd75ca7
adrien canino <adrien canino>
2019-04-21 12:06:39 +0200

    UX

--- ocs_video.js
+++ ocs_video.js
@@ -9,7 +9,8 @@ function createWindow() {
 		icon: 'assets/icons/logo.png',
 		movable: true,
 		backgroundColor: '#1D1D28', //In order to disable the white background color when we resize the window (instead of 'transparent: true')
-		frame: false //Borderless
+		frame: false, //Borderless
+		resizable: false
 	});
 
 	mainWindow.on('closed', () => {
